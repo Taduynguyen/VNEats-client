b3b5cf157762f3365b7256ed3f61d670febfa543
Taduynguyen <taduynguyen11@gmail.com>
2024-03-10 20:09:53 +0700

    Good

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,21 +1,96 @@
 import { useSearchOtherRestaurant } from "@/api/OtherRestaurantApi";
 import { useParams } from "react-router-dom";
+import LoadingPage from "./LoadingPage";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchResultCard from "@/components/SearchResultCard";
+import { useState } from "react";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import PaginationSelector from "@/components/PaginationSelector";
+import CuisineFilter from "@/components/CuisineFilter";
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+};
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results } = useSearchOtherRestaurant(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    page: 1,
+    selectedCuisines: [],
+  });
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const { results, isLoading } = useSearchOtherRestaurant(searchState, city);
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+      page: 1,
+    }));
+  };
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!results?.data || !city) {
+    return <span>Không có tìm kiếm phù hợp</span>;
+  }
 
   return (
-    <span>
-      Từ khóa tìm kiếm "{city}"
-      <span>
-        {results?.data.map((restaurant) => (
-          <span>
-            Tìm thấy - {restaurant.restaurantName}, {restaurant.city}
-          </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() => setIsExpanded((prevIsExpanded) => !prevIsExpanded)}
+        />
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Tìm kiếm món ăn hoặc tên nhà hàng"
+          onReset={resetSearch}
+        />
+        <SearchResultInfo city={city} total={results.pagination.total} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
-      </span>
-    </span>
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
   );
 };
 
